570d84a6382baa21f0c06638379bb5a01a57f0c8
felixg318 <sutro.sg29@gmail.com>
2025-11-15 16:17:55 -0500

    added batching

--- src/embedding.c
+++ src/embedding.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 const int STRIDE = 1;
-const int BATCH_SIZE = 32;
+const int BATCH_SEQ = 32;
 const int SEQ_LENGTH = 128;
 const int EMBEDDING_DIM = 64;
 
@@ -10,33 +10,34 @@ float random_float(float min, float max) {
 	return min + ((float)rand() / (float)RAND_MAX) * (max - min);
 }
 
-void create_embeddings(float *pos_data, int size, float min, float max) {
+void create_embedding(float *weights, int size, float min, float max) {
 	for (int i = 0; i < size; ++i) 
-		pos_data[i] = random_float(min, max);
+		weights[i] = random_float(min, max);
 }
 
-void create_batches(int *src, int *in_batch, int *target_batch, int src_size, int batches, int pad_id) {
-	int batch_size = BATCH_SIZE * SEQ_LENGTH;
+void create_batches(int *src, int *in_batch, int *target_batch, int src_size, int pad_id) {
+	for (int i = 0; i < BATCH_SEQ; ++i) {
+		for (int j = 0; j < SEQ_LENGTH; ++j) {
+			int idx = i * SEQ_LENGTH + j;
 
-	for (int b = 0; b < batches; ++b) {
-		for (int i = 0; i < BATCH_SIZE; ++i) {
-			for (int j = 0; j < SEQ_LENGTH; ++j) {
-				int idx = b * batch_size + i * SEQ_LENGTH + j;
-
-				in_batch[idx] = (idx < src_size) ? src[idx] : pad_id;
-				target_batch[idx] = (idx + STRIDE < src_size) ? src[idx + STRIDE] : pad_id;
-				
-			}
+			in_batch[idx] = (idx < src_size) ? src[idx] : pad_id;
+			target_batch[idx] = (idx + STRIDE < src_size) ? src[idx + STRIDE] : pad_id;
 		}
 	}
 }
 
-void embed_tokens(int *input, float *weights, float *out, int input_size, int vocab_size, int embedding_dim) {
-	int embedded_tokens_size = input_size * embedding_dim;
-
+//can cause bounds checking error
+void embed_tokens(int *input, float *weights, float *out, int input_size, int embedding_dim) {
 	for (int token = 0; token < input_size; ++token) 
 		for (int j = 0; j < embedding_dim; ++j) 
 			out[token * embedding_dim + j] = weights[input[token] * embedding_dim + j];
+}
+
+//tokens acts like a lookup for pos_data
+void inplace_add_positional(float *embedding, int *tokens, float *pos_data, int batch_size, int embedding_dim) {
+	for (int i = 0; i < batch_size; ++i) 
+		for (int j = 0; j < embedding_dim; ++j) 
+			embedding[i * embedding_dim + j] += pos_data[tokens[i] * embedding_dim + j];
 }
 
 void print_array(int *a, int size) {
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,14 @@
 #include "tokenizor.hpp"
 
 const int STRIDE = 1;
-const int BATCH_SIZE = 32;
+const int BATCH_SEQ = 32;
 const int SEQ_LENGTH = 128;
 const int EMBEDDING_DIM = 64;
 
-void create_batches (int *src, int *in_batch, int *target_batch, int src_size, int batches, int pad_id);
-void create_embeddings (float *weights, int size, float min, float max); 
-void embed_tokens (int *input, float *weights, float *out, int input_size, int vocab_size, int embedding_dim);
+void create_batches (int *src, int *in_batch, int *target_batch, int size, int pad_id);
+void create_embedding (float *weights, int size, float min, float max); 
+void embed_tokens (int *input, float *weights, float *out, int batch_size, int embedding_dim);
+void inplace_add_positional(float *embedding, int *tokens, float *pos_weights, int batch_size, int embedding_dim);
 void mat_inplace_add (float *A, float *B, int rows, int cols);
 void print_array (int *a, int size);
 void print_farray (float *a, int size);
@@ -17,11 +18,11 @@ void save_embedded_vectors (float *arr, int x, int y);
 int main(int argc, char** argv) {
 	Tokenizor tokenizor;
 	int *input_batch, *target_batch;
-	float *embedding_weight_layer, *embedded_tokens, *pos_data;
-	int batch_size = BATCH_SIZE * SEQ_LENGTH;
+	float *token_weights, *embedding_batch, *pos_weights;
+	int batch_size = BATCH_SEQ * SEQ_LENGTH;
 	int embedding_dim = EMBEDDING_DIM;
 	int batches; 
-	int vocab_size, data_size, embedding_size, embedded_tokens_size;
+	int vocab_size, data_size, embedding_size, token_weights_size;
 	int pad_id; 
 	if (argc < 2 || argc > 2) {
 		std::cout << "Bad args" << '\n';
@@ -45,42 +46,45 @@ int main(int argc, char** argv) {
 	
 	vocab_size = tokenizor.vocab_size;
 
-	embedding_size = vocab_size * embedding_dim;
-	embedded_tokens_size = data_size * embedding_dim;
+	token_weights_size = vocab_size * embedding_dim;
+	embedding_size = data_size * embedding_dim;
 	
-	input_batch = new int[batches * batch_size]; 
-	target_batch = new int[batches * batch_size];
+	input_batch = new int[batch_size]; 
+	target_batch = new int[batch_size];
+	embedding_batch = new float[batch_size * embedding_dim];
 	
-	embedding_weight_layer = new float[embedding_size]; 
+	token_weights = new float[token_weights_size]; 
+	pos_weights = new float[embedding_size];
+
+	std::cout<< batches << " batches of " << BATCH_SEQ << " x " << SEQ_LENGTH << " tokens\n";
 	
-	embedded_tokens = new float[embedded_tokens_size];
-	pos_data = new float[embedded_tokens_size];
+	create_embedding(token_weights, token_weights_size, -5.0, 5.0);
+	create_embedding(pos_weights, embedding_size, -0.05, 0.05);
 
-	std::cout<< batches << " batches of " << BATCH_SIZE << " x " << SEQ_LENGTH << " tokens\n";
-
+	//potentially buggy
+	//add bounds checking
+	//to add to gpu, load weights and positional data to memory for cuda kernels
+	for (int b = 0; b < batches; b++) {
+		int batch_offset = b * batch_size;
+		
+		create_batches(tokenizor.data.data() + batch_offset, input_batch, target_batch, batch_size, pad_id);
+		
+		embed_tokens(input_batch, token_weights, embedding_batch, batch_size, embedding_dim);
+		inplace_add_positional(embedding_batch, input_batch, pos_weights, batch_size, embedding_dim);
 	
-	create_embeddings(embedding_weight_layer, embedding_size, -5.0, 5.0);
-	create_embeddings(pos_data, embedded_tokens_size, -0.05, 0.05);
+	}
 
-//	Batching is going to be a huge pain in the ass when we implement MPI since
-//	technically embedding tokens is to be done in batches
 
-	create_batches(tokenizor.data.data(), input_batch, target_batch, data_size, batches, pad_id);
-	
-
-	embed_tokens(tokenizor.data.data(), embedding_weight_layer, embedded_tokens, data_size, vocab_size, embedding_dim);
-
-	mat_inplace_add(embedded_tokens, pos_data, data_size, embedding_dim); //safe to assume same dimensions 
-
+//	For use later:
 //	save_embedded_vectors(embedded_tokens, embedding_dim, data_size);
-
 //	tokenizor.decode(array, size);
-	
+
 	delete[] input_batch;
 	delete[] target_batch;
-	delete[] embedding_weight_layer;
-	delete[] pos_data;
-	delete[] embedded_tokens;
+	delete[] embedding_batch;
+
+	delete[] token_weights;
+	delete[] pos_weights;
 
 	return 0;
 }
